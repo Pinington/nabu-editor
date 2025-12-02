cc8a8e6b26515f9c600d090eeb6bf8b2014bc046
Pinington <georges5757c@gmail.com>
2025-12-02 23:05:08 +0100

    Mouse clicks and new lines
    Made mouse clicks change cursor position, overwrite text if there is any and be able to backspace. Aditionally made new lines create a blank line instead of emptying the line below to write

--- src/editor.c
+++ src/editor.c
@@ -53,6 +53,8 @@ int get_column_from_x(TTF_Font *font, const char *text, int x) {
     int col = 0;
     int cw = 0;
 
+    // The for loop stopping at the last character insures we get 
+    // the last char's position if we click in empty space to the right
     for (int i = 0; text[i] != '\0'; i++) {
         // Measure next character
         char temp[2] = { text[i], '\0' };
@@ -69,23 +71,51 @@ int get_column_from_x(TTF_Font *font, const char *text, int x) {
 }
 
 
-int render_line(struct Display* display, int row, char* txt, SDL_Color* color, TTF_Font** font) {
+int text_too_wide(TTF_Font *font, const char *txt) {
+    int w = 0;
+    TTF_SizeText(font, txt, &w, NULL);
+    return w > WINDOW_WIDTH - FONT_SIZE; // Enough space at the end for one character
+}
+
+
+void render_line(struct Display* display, int row, char* txt, SDL_Color* color, TTF_Font** font) {
+    // Create the surface of the message
     SDL_Surface* surface_message =
         TTF_RenderText_Blended(*font, txt, *color); 
 
+    // Make surface into message
     SDL_Texture* Message = SDL_CreateTextureFromSurface(display->renderer, surface_message);
 
+    // Create the rectangle that will contain the text and position it
     SDL_Rect Message_rect; 
     Message_rect.x = 10; 
     Message_rect.y = 10 + row * TTF_FontLineSkip(*font);
     SDL_QueryTexture(Message, NULL, NULL, &Message_rect.w, &Message_rect.h);
 
+    // Pass the restangle and the message to the renderer
     SDL_RenderCopy(display->renderer, Message, NULL, &Message_rect);
-    SDL_FreeSurface(surface_message);
 
+    // Free all the useless stuff
+    SDL_FreeSurface(surface_message);
     SDL_DestroyTexture(Message);
-    if (Message_rect.w > WINDOW_WIDTH - 48) return 1;
-    return 0;
+}
+
+
+void handle_new_line
+(int* cursor_row, int* lines, int buffer_size[MAX_LINES], char buffer[MAX_LINES][BUFFER_SIZE]) {
+    // Create an empty line below current line and displace all the ones below (linked lists could''ve been useful fr)
+    (*cursor_row)++;
+    (*lines)++;
+
+    for (int i = *lines; i > *cursor_row; i--) {
+        for (int j = 0; j < buffer_size[i] + 1; j++) {
+            strcpy(buffer[i], buffer[i - 1]);
+            buffer_size[i] = buffer_size[i - 1];
+        }
+    }
+
+    buffer[*cursor_row][0] = '\0';
+    buffer_size[*cursor_row] = 0;
 }
 
 
@@ -113,11 +143,11 @@ void start_console() {
     int buffer_size[MAX_LINES] = {0};
 
     // Initalize the cursor position to be on the first line
+    int cursor_col = 0;
     int cursor_row = 0;
     int lines = 0;
 
     int running = 1;
-    int newline_flag = 0;
     while (running) {
         SDL_Event event;
         while (SDL_PollEvent(&event)) {
@@ -130,48 +160,68 @@ void start_console() {
                 case SDL_MOUSEBUTTONDOWN:
                     int row = (event.button.y - 10) / TTF_FontLineSkip(Font);
                     int col = get_column_from_x(Font, buffer[row], event.button.x - 10);
-                    printf("R: %i, C:%i\n", row, col);
                     if (row <= lines && col < BUFFER_SIZE) {
                         cursor_row = row;
-                        buffer_size[cursor_row] = col;
+                        cursor_col = col;
                     } else {
+                        // If I click in the empty space below text, set back cursor to last character in my text
+                        // Might cause errors if somehow I get row < 0 or something
                         cursor_row = lines;
-                        buffer_size[cursor_row] = strlen(buffer[cursor_row]);
+                        cursor_col = strlen(buffer[cursor_row]);
                     }
                     break;
+
 
                 // Handle sepcial key presses
                 case SDL_KEYDOWN:
                     if (event.key.keysym.scancode == SDL_SCANCODE_ESCAPE) {
                         running = 0;
+
                     } else if (event.key.keysym.scancode == SDL_SCANCODE_BACKSPACE) {
-                        if (buffer_size[cursor_row] > 0) {
-                            buffer[cursor_row][--buffer_size[cursor_row]] = '\0';
+                        if (cursor_col > 0) {
+                            // If line isnt empty remove char from array
+                            for (int i = cursor_col - 1; i < buffer_size[cursor_row]; i++) {
+                                buffer[cursor_row][i] = buffer[cursor_row][i + 1];
+                            }
+                            buffer_size[cursor_row]--;
+                            cursor_col--;
                         } else if (cursor_row > 0) {
+                            // If line is empty, and it's not the first line go the line above
                             cursor_row--;
-                            buffer[cursor_row][--buffer_size[cursor_row]] = '\0';
+                            cursor_col = --buffer_size[cursor_row];
+                            buffer[cursor_row][cursor_col] = '\0';
                         }
+                        
                     } else if (event.key.keysym.scancode == SDL_SCANCODE_RETURN) {
-                        if (cursor_row < MAX_LINES) cursor_row++;
-                        lines = MAX(lines, cursor_row);
+                        if (lines < MAX_LINES - 1) {
+                            handle_new_line(&cursor_row, &lines, buffer_size, buffer);
+                            cursor_col = 0;
+                        }
                     } 
                     break;
+
 
                 // Handle text input
                 case SDL_TEXTINPUT:
                     for (int i = 0; event.text.text[i]; i++) {
-                        if (newline_flag) {
-                            cursor_row++;
-                            lines = MAX(lines, cursor_row);
-                            newline_flag = 0;
-                            buffer[cursor_row][buffer_size[cursor_row]] = '\0';
-                        }
+                        buffer[cursor_row][cursor_col++] = event.text.text[i];
+                        buffer[cursor_row][++buffer_size[cursor_row]] = '\0';
 
-                        buffer[cursor_row][buffer_size[cursor_row]++] = event.text.text[i];
-                        buffer[cursor_row][buffer_size[cursor_row]] = '\0';
-                        if (buffer_size[cursor_row] > BUFFER_SIZE) {
-                            cursor_row++;
-                            lines = MAX(lines, cursor_row);
+                        if (text_too_wide(Font, buffer[cursor_row]) && lines < MAX_LINES - 1) {
+
+                            // Save last character and then set it to null terminator in the buffer
+                            char last = buffer[cursor_row][cursor_col - 1];
+                            buffer_size[cursor_row]--;
+                            buffer[cursor_row][buffer_size[cursor_row]] = '\0';
+                            
+                            handle_new_line(&cursor_row, &lines, buffer_size, buffer);
+
+                            // Set first charcter of the new line to be the one saved
+                            buffer_size[cursor_row] = 1;
+                            buffer[cursor_row][0] = last;
+                            buffer[cursor_row][1] = '\0';
+
+                            cursor_col = 1;
                         }
                     }
                     break; 
@@ -186,15 +236,13 @@ void start_console() {
         // TTF text section
         
         for (int i = 0; i <= lines; i++) {
+            // I'm gonna try and make it more efficient by creating big messages
             render_line(&display, i, buffer[i], &Color, &Font);
         }
-
-        if (render_line(&display, cursor_row, buffer[cursor_row], &Color, &Font)) newline_flag = 1;
 
         // End of TTF section
 
         SDL_RenderPresent(display.renderer);
-
         SDL_Delay(1000 / FPS);
     }
     
