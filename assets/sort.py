06cfb12e49298d93ccceb1a810712fab72c7ade9
Pinington <georges5757c@gmail.com>
2026-01-17 16:41:37 +0100

    Cleaned words databse

--- assets/sort.py
+++ assets/sort.py
@@ -1,4 +1,4 @@
-words = open("unsorted_words.txt").read().split()
+words = open("assets/unsorted_words.txt").read().split()
 words.sort(key=len)
-with open("by_length.txt", "w") as f:
+with open("assets/words.txt", "w") as f:
     f.write("\n".join(words))
